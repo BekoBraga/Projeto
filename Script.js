756149b9cc715ff00b3e030859997eb967ffa891
BekoBraga <65620900+BekoBraga@users.noreply.github.com>
2020-06-28 08:24:43 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,56 @@
+let medicamentoUso = '';
+let alergicos = '';
+let alteraCardi = '';
+let distCircula = '';
+let distRenal = '';
+let distHormonal = '';
+let distGasItestinal = '';
+let eplConvulsao = '';
+let altPsco = '';
+let estressObs = '';
+let anteceAmcolo = '';
+let diabetes = '';
+let algTipoDoença = '';
+
+
+function capturar () {
+    medicamentoUso = document.getElementById('fname').value;
+    document.getElementById('fname').innerHTML = medicamentoUso;
+}
+
+function verificar(){
+    
+if((anamnese.a[0].checked==false) && (anamnese.a[1].checked==false))
+        console.log('medicamentoUso');
+elseif((anamnese.b[0].checked==false) && (anamnese.b[1].checked==false))
+alert("blaealala");
+elseif((anamnese.c[0].checked==false) && (anamnese.c[1].checked==false)) 
+alert("blala");
+elseif((anamnese.d[0].checked==false) && (anamnese.d[1].checked==false)) 
+alert("blala");
+elseif((anamnese.e[0].checked==false) && (anamnese.e[1].checked==false)) 
+alert("blaalala");
+elseif((anamnese.f[0].checked==false) && (anamnese.f[1].checked==false)) 
+alert("blaalala");
+elseif((anamnese.g[0].checked==false) && (anamnese.g[1].checked==false)) 
+alert("blalala");
+elseif ((anamnese.h[0].checked==false) && (anamnese.h[1].checked==false)) 
+alert("blalala");
+elseif((anamnese.i[0].checked==false) && (anamnese.i[1].checked==false))
+alert("blaal34al");
+elseif((anamnese.j[0].checked==false) && (anamnese.j[1].checked==false))
+alert("bl3aaala");
+elseif((anamnese.l[0].checked==false) && (anamnese.l[1].checked==false))
+alert("bl3aaala");
+elseif((anamnese.m[0].checked==false) && (anamnese.m[1].checked==false))
+alert("bl3aaala");
+elseif((anamnese.n[0].checked==false) && (anamnese.n[1].checked==false))
+alert("bl3aaala");
+elseif((anamnese.o[0].checked==false) && (anamnese.o[1].checked==false))
+alert("bl3aaala");
+elseif((anamnese.p[0].checked==false) && (anamnese.p[1].checked==false))
+alert("bl3aaala");
+
+}
+
+
